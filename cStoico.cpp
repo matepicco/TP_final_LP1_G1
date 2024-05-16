2fd5b77a5f967d15680c61a37dc9ba785abe47a4
Nicanor García Saavedra <nicanorgs27@gmail.com>
2024-05-16 15:30:03 -0300

    ahora si stoico tambien esta en la master

---
+++ cStoico.cpp
@@ -0,0 +1,25 @@
+#include "cStoico.h"
+
+cStoico::cStoico()
+{
+	this->listaVikingos = list<cVikingo*>(); //auspicia mati larrosa
+}
+
+void cStoico::agregarVikingo(cVikingo& objV)
+{
+	this->listaVikingos.push_back(&objV);
+}
+
+void cStoico::eliminarVikingo(cVikingo* objV)
+{
+	this->listaVikingos.pop_back(); //?
+}
+
+int cStoico::getcantVikCreados()
+{
+	return cantVikCreados;
+}
+
+cStoico::~cStoico()
+{
+}
---
+++ cStoico.h
@@ -0,0 +1,16 @@
+#pragma once
+#include "cVikingo.h"
+
+class cStoico
+{
+private:
+	static int cantVikCreados;
+	list <cVikingo*> listaVikingos;
+public:
+	cStoico();
+	void agregarVikingo(cVikingo& objV);
+	void eliminarVikingo(cVikingo* objV);
+	static int getcantVikCreados();
+	~cStoico();
+
+};
